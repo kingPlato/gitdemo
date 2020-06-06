443a140944789ee50b2c5adfa336c9607c15323c
qtyb <wggld@sina.com>
2020-06-06 21:51:36 +0800

    ElectronicImpl modified

--- ElectronicImpl.java
+++ ElectronicImpl.java
@@ -2,7 +2,7 @@ package com.asiainfo.test;
 
 public class ElectronicImpl {
 	public String eiInterface(String arg0) throws Exception{
-		System.out.println(arg0);
+		System.out.println(arg0+"test");
         return arg0;
     }
 }
