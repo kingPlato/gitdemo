5ff29d38aa6412dd406e69bac43d869cec32210d
kingPlato <qtyb920@gmail.com>
2020-06-07 23:56:55 +0800

    idea commit

--- ElectronicImpl.java
+++ ElectronicImpl.java
@@ -3,7 +3,7 @@ package com.asiainfo.test;
 public class ElectronicImpl {
 	public String eiInterface(String arg0) throws Exception{
 		System.out.println(arg0+"test branch");
-		System.out.println(arg0+"lkjalkjdflajksdf");
+		System.out.println(arg0+"lkjalkjd34234flajksdf");
 
         return arg0;
     }
